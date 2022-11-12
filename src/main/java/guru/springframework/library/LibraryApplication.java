0f098beb00f393bbf11daeb8394137e1b8e66415
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-11-12 11:13:39 -0300

    create library project

---
+++ src/main/java/guru/springframework/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
