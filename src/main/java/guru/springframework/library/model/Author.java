5cc98b63c9e152193eb0fa2fc284283ab916fc86
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-11-12 14:18:22 -0300

    [FEAT] override equals and hashcode method to set equality based on object id

--- src/main/java/guru/springframework/library/model/Author.java
+++ src/main/java/guru/springframework/library/model/Author.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -33,5 +34,20 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.bookSet = bookSet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
--- src/main/java/guru/springframework/library/model/Book.java
+++ src/main/java/guru/springframework/library/model/Book.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 @Entity
 public class Book {
@@ -35,5 +36,20 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.authorSet = authorSet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
