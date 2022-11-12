bdbd20efcd0e39b8ed44506de32e849ebd688124
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-11-12 12:58:43 -0300

    [FEAT] add entity annotation to models

--- src/main/java/guru/springframework/library/model/Author.java
+++ src/main/java/guru/springframework/library/model/Author.java
@@ -3,15 +3,33 @@ package guru.springframework.library.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Getter @Setter
+    private Long id;
     @Getter @Setter
     private String firstName;
     @Getter @Setter
     private String lastName;
     @Getter @Setter
     private Set<Book> bookSet;
+
+    public Author(){}
+
+    public Author(Long id,String firstName,String lastName, Set<Book> bookSet){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.bookSet = bookSet;
+    }
 
     public Author(String firstName,String lastName, Set<Book> bookSet){
         this.firstName = firstName;
--- src/main/java/guru/springframework/library/model/Book.java
+++ src/main/java/guru/springframework/library/model/Book.java
@@ -3,15 +3,32 @@ package guru.springframework.library.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
-
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter @Setter
+    private Long id;
     @Getter @Setter
     private String title;
     @Getter @Setter
     private String isbn;
     @Getter @Setter
     private Set<Author> authorSet;
+
+    public Book(){}
+
+    public Book(Long id,String title,String isbn,Set<Author> authorSet){
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+        this.authorSet = authorSet;
+    }
 
     public Book(String title,String isbn,Set<Author> authorSet){
         this.title = title;
