edc9b3ca537c43489fc2cfbe56eadb8652242625
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-11-16 20:31:42 -0300

    [FEAT] add publisher controller

---
+++ src/main/java/guru/springframework/library/controller/PublisherController.java
@@ -0,0 +1,24 @@
+package guru.springframework.library.controller;
+
+import guru.springframework.library.repository.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class PublisherController {
+
+    private final BookRepository bookRepository;
+
+    public PublisherController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getAllBook(Model model){
+        model.addAttribute("book",bookRepository.findAll());
+        return null;
+    }
+}
