f6623a63b751b06342f8daa3543bfc11592ad44b
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-11-23 15:25:31 -0300

    add greetingService english implementation

---
+++ src/main/java/guru/springframework/library/service/greeting/GreetingServiceEnglish.java
@@ -0,0 +1,11 @@
+package guru.springframework.library.service.greeting;
+
+import guru.springframework.library.service.greeting.GreetingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingServiceEnglish implements GreetingService {
+    public String sayGreeting() {
+        return "Hello, World";
+    }
+}
