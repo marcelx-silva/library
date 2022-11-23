210c0917ee0190286dd236693d9941f7281e1d67
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-11-23 15:42:54 -0300

    add service annotation

--- src/main/java/guru/springframework/library/service/greeting/GreetingServicePolish.java
+++ src/main/java/guru/springframework/library/service/greeting/GreetingServicePolish.java
@@ -1,5 +1,8 @@
 package guru.springframework.library.service.greeting;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingServicePolish implements GreetingService{
     @Override
     public String sayGreeting() {
