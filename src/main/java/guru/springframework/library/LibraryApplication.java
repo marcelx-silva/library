f351aaee71d8da9b6ef7a1da495270785155cd57
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-11-23 16:50:17 -0300

    main

--- src/main/java/guru/springframework/library/LibraryApplication.java
+++ src/main/java/guru/springframework/library/LibraryApplication.java
@@ -1,13 +1,23 @@
 package guru.springframework.library;
 
+import guru.springframework.library.controller.greeting.GreetingEnglishController;
+import guru.springframework.library.controller.greeting.GreetingGermanController;
+import guru.springframework.library.controller.greeting.GreetingPolishController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LibraryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LibraryApplication.class, args);
+        ApplicationContext context = SpringApplication.run(LibraryApplication.class, args);
+        GreetingEnglishController greetingEnglishController = (GreetingEnglishController) context.getBean("greetingEnglishController");
+        GreetingGermanController greetingGermanController = (GreetingGermanController) context.getBean("greetingGermanController");
+        GreetingPolishController greetingPolishController = (GreetingPolishController) context.getBean("greetingPolishController");
+        System.out.println("English Greeting: "+greetingEnglishController.getGreeting());
+        System.out.println("Polish Greeting: "+greetingPolishController.getGreeting());
+        System.out.println("German Greeting: "+greetingGermanController.getGreeting());
     }
 
 }
